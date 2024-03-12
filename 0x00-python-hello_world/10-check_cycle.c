33922a00c08a1d025831257ea2f5157a360a9c16
Abel Tadesse <138736916+abimtad@users.noreply.github.com>
2024-03-12 15:18:10 +0300

    Update 10-check_cycle.c

--- 0x00-python-hello_world/10-check_cycle.c
+++ 0x00-python-hello_world/10-check_cycle.c
@@ -1,31 +1,24 @@
+#include <stddef.h>
 #include "lists.h"
 
 /**
- * check_cycle - check for loop in LL
+ * check_cycle - check for loop in Linked list
  * @list: head of linked list
  *
- * Description - check for loops in LL
- * Return: 1 if cycled, 0 if not
+ * Return: 1 if cycled, 0 otherwise
+ * Description - check for loops in a Linked list
  */
-
 int check_cycle(listint_t *list)
 {
-	listint_t *slow, *fast;
+	listint_t *curr_node = list;
 
-	if (!list)
+	while (curr_node != NULL)
 	{
-		return (0);
-	}
-	slow = list;
-	fast = list->next;
-	while (fast && slow && fast->next)
-	{
-		if (slow == fast)
-		{
+		if (curr_node <= curr_node->next)
 			return (1);
-		}
-		slow = slow->next;
-		fast = fast->next->next;
+
+		curr_node = curr_node->next;
 	}
+
 	return (0);
 }
