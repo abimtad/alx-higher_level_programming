01322c8354b138aaa4170db3616614e961d7cb72
Abel Tadesse <138736916+abimtad@users.noreply.github.com>
2024-03-14 00:36:02 +0300

    Update 7-islower.py

--- 0x01-python-if_else_loops_functions/7-islower.py
+++ 0x01-python-if_else_loops_functions/7-islower.py
@@ -1,7 +1,7 @@
 #!/usr/bin/python3
 def is_lower(c):
     """Checks whether a character c is lower and returns a boolean value"""
-    for char in range(97, 123):
-        if ord(c) == char:
-            return True
-    return False
+    if ord(c) in range(97, 123):
+        return True
+    else;
+        False
