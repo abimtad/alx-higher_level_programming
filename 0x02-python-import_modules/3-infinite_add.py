2141465f0e6783bfd561df222d913a806b940ef1
Abel Tadesse <tadesseabel26@gmail.com>
2024-03-14 23:42:29 +0300

    created a script file.

---
+++ 0x02-python-import_modules/3-infinite_add.py
@@ -0,0 +1,9 @@
+#!/usr/bin/python3
+if __name__ == "__main__":
+    """prints the result of the sum of all the arguments"""
+    import sys
+
+    result = 0
+    for args in range(1, len(sys.argv)):
+        result+=int(sys.argv[args])
+    print(result)
