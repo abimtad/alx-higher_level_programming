d73492359cb08712498c50b44b614f6223ad1205
Abel Tadesse <tadesseabel26@gmail.com>
2024-03-19 02:26:24 +0300

    updated it.

--- 0x02-python-import_modules/4-hidden_discovery.py
+++ 0x02-python-import_modules/4-hidden_discovery.py
@@ -1,7 +1,7 @@
 #!/usr/bin/python3
 if __name__ == "__main__":
     import hidden_4
-
-    for j in dir(hidden_4):
-        if j[0:2] != '__':
-            print(i)
+    list = dir(hidden_4)
+    for a in list:
+        if a[0] != '_':
+            print("{}".format(a))
