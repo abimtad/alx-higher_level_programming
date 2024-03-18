77b5d2dd64d94460f4141d5c9a379a42c2614c99
Abel Tadesse <138736916+abimtad@users.noreply.github.com>
2024-03-19 02:02:51 +0300

    Update 6-print_matrix_integer.py

--- 0x03-python-data_structures/6-print_matrix_integer.py
+++ 0x03-python-data_structures/6-print_matrix_integer.py
@@ -1,16 +1,13 @@
 #!/usr/bin/python3
 def print_matrix_integer(matrix=[[]]):
     """
-    The function prints the integers given int the -
-    matrix as a matrix of integers.
+    The function prints the integers given in the matrix as a matrix of integers.
 
     :param matrix: The two-dimensional list to print.
 
     :return: None.
     """
     for row in matrix:
-        for element in row:
-            if row.index(element) != len(row) - 1:
-                print('{:d}'.format(element), end=' ')
-            else:
-                print('{:d}'.format(element))
+        for i in range(len(row)):
+            print('{:d}'.format(row[i]), end=' ')
+        print()
