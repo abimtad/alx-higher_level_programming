830c75e30e46aee018a94d00160b3497f784bd38
Abel Tadesse <tadesseabel26@gmail.com>
2024-03-19 02:20:50 +0300

    created a file.

--- 0x03-python-data_structures/6-print_matrix_integer.py
+++ 0x03-python-data_structures/6-print_matrix_integer.py
@@ -1,6 +1,9 @@
 #!/usr/bin/python3
-def print_reversed_list_integer(my_list=[]):
-    """prints all integers of a list, in reverse order."""
-    if isinstance(my_list, list):
-        for i in reversed(my_list):
-            print("{:d}".format(i))
+def print_matrix_integer(matrix=[[]]):
+    for n in range(len(matrix)):
+        for m in range(len(matrix[n])):
+            print("{:d}".format(matrix[n][m]), end="")
+            if m != (len(matrix[n]) - 1):
+                print(" ", end="")
+
+        print("")
