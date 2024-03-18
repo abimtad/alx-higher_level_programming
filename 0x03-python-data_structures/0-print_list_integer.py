23a0c4239fd5f808ace9ddba822e95a9f4e87c6c
Abel Tadesse <138736916+abimtad@users.noreply.github.com>
2024-03-18 18:10:53 +0300

    Create 0-print_list_integer.py

---
+++ 0x03-python-data_structures/0-print_list_integer.py
@@ -0,0 +1,13 @@
+#! /usr/bin/python3
+def print_list_integer(my_list=[]):
+    """
+    This function prints the integer values of list, each in a new line.
+
+    Parameters:
+    my_list: The list containing the integer numbers.
+
+    Return: None
+    """
+
+    for integer_idx in range(len(my_list)):
+        print('{:d}'.format(my_list[integer_idx]))
