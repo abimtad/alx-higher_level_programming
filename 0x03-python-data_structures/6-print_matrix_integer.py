9de71cc900e66d8a6b82bcd68ac1b8b69bfb03b6
Abel Tadesse <138736916+abimtad@users.noreply.github.com>
2024-03-19 02:16:15 +0300

    Update 6-print_matrix_integer.py

--- 0x03-python-data_structures/6-print_matrix_integer.py
+++ 0x03-python-data_structures/6-print_matrix_integer.py
@@ -1,16 +1,6 @@
 #!/usr/bin/python3
-def print_matrix_integer(matrix=[[]]):
-    """
-    The function prints the integers given int the matrix as a
-    matrix of integers.
-
-    :param matrix: The two-dimensional list to print.
-
-    :return: None.
-    """
-    for row in matrix:
-        for element in row:
-            if row.index(element) != len(row) - 1:
-                print('{:d}'.format(element), end=' ')
-            else:
-                print('{:d}'.format(element))
+def print_reversed_list_integer(my_list=[]):
+    """prints all integers of a list, in reverse order."""
+    if isinstance(my_list, list):
+        for i in reversed(my_list):
+            print("{:d}".format(i))
