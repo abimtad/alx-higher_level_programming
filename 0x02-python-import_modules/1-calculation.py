de462bdadc5aabe3272e67e19ff7a93ab056faef
Abel Tadesse <138736916+abimtad@users.noreply.github.com>
2024-03-19 18:34:30 +0300

    Update 1-calculation.py

--- 0x02-python-import_modules/1-calculation.py
+++ 0x02-python-import_modules/1-calculation.py
@@ -1,10 +1,8 @@
 #!/usr/bin/python3
-if __name__ == "__main__":
-    from calculator_1 import sub, add, mul, div
-    """A calculator"""
-
     a = 10
     b = 5
+if __name__ == "__main__":
+    from calculator_1 import add, sub, mul, div
     print("{0:d} + {1:d} = {2:d}".format(a, b, add(a, b)))
     print("{0:d} - {1:d} = {2:d}".format(a, b, sub(a, b)))
     print("{0:d} * {1:d} = {2:d}".format(a, b, mul(a, b)))
