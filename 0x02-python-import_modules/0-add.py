34e74275ff5846532eb31043fe098afa0f8ad251
Abel Tadesse <138736916+abimtad@users.noreply.github.com>
2024-03-19 18:31:46 +0300

    Update 0-add.py

--- 0x02-python-import_modules/0-add.py
+++ 0x02-python-import_modules/0-add.py
@@ -1,7 +1,6 @@
 #!/usr/bin/python3
+from add_0 import add
+a = 1
+b = 2
 if __name__ == "__main__":
-    """sum of a = 1 and b = 2"""
-    from add_0 import add
-    a = 1
-    b = 2
     print("{0:d} + {1:d} = {2:d}".format(a, b, add(a, b)))
