157b840edf2e62237f693736b4bc0c6a6fe4e696
Abel Tadesse <138736916+abimtad@users.noreply.github.com>
2024-03-19 18:54:21 +0300

    Update 2-args.py

--- 0x02-python-import_modules/2-args.py
+++ 0x02-python-import_modules/2-args.py
@@ -1,12 +1,13 @@
 #!/usr/bin/python3
+from sys import argv
+
 if __name__ == "__main__":
-    from sys import argv
     arguments = argv[1:]
     num_arguments = len(arguments)
 
     if num_arguments == 0:
         print("0 arguments.")
     else:
-        print(f"{num_arguments} arguments:")
+        print("{:d} argument{:s}:".format(num_arguments, 's' if num_arguments > 1 else ''))
         for idx, arg in enumerate(arguments, 1):
             print(f"{idx}: {arg}")
