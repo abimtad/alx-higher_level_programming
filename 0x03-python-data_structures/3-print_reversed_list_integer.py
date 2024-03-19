bedfcee12796fcb25e6e02231742282b1d0d27b2
Abel Tadesse <138736916+abimtad@users.noreply.github.com>
2024-03-19 18:01:01 +0300

    Update 3-print_reversed_list_integer.py

--- 0x03-python-data_structures/3-print_reversed_list_integer.py
+++ 0x03-python-data_structures/3-print_reversed_list_integer.py
@@ -1,7 +1,8 @@
 #!/usr/bin/python3
 def print_reversed_list_integer(my_list=[]):
     """
-    The function prints the integers in the provided list in a reversed order, each on a new line.
+    The function prints the integers in the provided list in a reversed
+    order, each on a new line.
 
     :param my_list: The list
 
