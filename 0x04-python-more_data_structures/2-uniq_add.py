71ace0d0590555149d03897458e51e304c6468a5
Abel Tadesse <tadesseabel26@gmail.com>
2024-03-21 01:15:26 +0300

    update logic.

--- 0x04-python-more_data_structures/2-uniq_add.py
+++ 0x04-python-more_data_structures/2-uniq_add.py
@@ -1,11 +1,11 @@
 #!/usr/bin/python3
 
 def uniq_add(my_list=[]):
-    """adds all unique integers in a list."""
-    a = []
-    result = 0
+    """adds all unique integers in b list."""
+    b = []
+    resultt = 0
     for i in range(len(my_list)):
-        if my_list[i] not in a:
-            a.append(my_list[i])
-            result += my_list[i]
-    return result
+        if my_list[i] not in b:
+            b.append(my_list[i])
+            resultt += my_list[i]
+    return resultt
