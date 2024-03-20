3ac0575674fcd3e9ac9b8314af48556053da8072
Abel Tadesse <tadesseabel26@gmail.com>
2024-03-21 01:22:44 +0300

    update logic.

--- 0x04-python-more_data_structures/100-weight_average.py
+++ 0x04-python-more_data_structures/100-weight_average.py
@@ -1,9 +1,8 @@
 #!/usr/bin/python3
-
 def weight_average(my_list=[]):
     mul_list = [x[0] * x[1] for x in my_list]
-    freq_list = [x[1] for x in my_list]
-    sum_list = sum(mul_list)
-    if sum(freq_list) == 0:
+    frequent_list = [x[1] for x in my_list]
+    sumed_list = sum(mul_list)
+    if sum(frequent_list) == 0:
         return 0
-    return sum_list / sum(freq_list)
+    return sumed_list / sum(frequent_list)
