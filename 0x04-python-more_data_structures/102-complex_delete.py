35bb79781abba66e0991a316b58ca174a8f059ee
Abel Tadesse <tadesseabel26@gmail.com>
2024-03-21 01:23:48 +0300

    update logic.

--- 0x04-python-more_data_structures/102-complex_delete.py
+++ 0x04-python-more_data_structures/102-complex_delete.py
@@ -4,8 +4,8 @@ def complex_delete(a_dictionary, value):
     if value not in a_dictionary.values():
         return a_dictionary
     while value in a_dictionary.values():
-        for k, v in a_dictionary.items():
-            if v == value:
-                del a_dictionary[k]
+        for a, b in a_dictionary.items():
+            if b == value:
+                del a_dictionary[a]
                 break
     return a_dictionary
