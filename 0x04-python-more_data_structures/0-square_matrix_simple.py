98afe92093061b39d30fb36525be30535b124a62
Abel Tadesse <138736916+abimtad@users.noreply.github.com>
2024-03-21 10:27:49 +0300

    Update 0-square_matrix_simple.py

--- 0x04-python-more_data_structures/0-square_matrix_simple.py
+++ 0x04-python-more_data_structures/0-square_matrix_simple.py
@@ -1,6 +1,13 @@
 #!/usr/bin/python3
 def square_matrix_simple(matrix=[]):
-    squared_matrix = []
-    for j in matrix:
-        squared_matrix.append(list(map(lambda y: y**2, j)))
-    return (squared_matrix)
+    """
+    The function squares every element of the matrix and returns a new matrix.
+
+    :param matrix: The two-dimensional matrix.
+
+    :return: A new squared matrix.
+    """
+    if isinstance(matrix, list):
+        new_matrix = list(map(lambda _list: [x**2 for x in _list], matrix))
+
+    return new_matrix
