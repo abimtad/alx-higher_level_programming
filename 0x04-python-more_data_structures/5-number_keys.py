094f5f924896b1c25a2cfb0b7d6ab524325c93c1
Abel Tadesse <138736916+abimtad@users.noreply.github.com>
2024-03-21 11:44:22 +0300

    Update 5-number_keys.py

--- 0x04-python-more_data_structures/5-number_keys.py
+++ 0x04-python-more_data_structures/5-number_keys.py
@@ -1,4 +1,11 @@
 #!/usr/bin/python3
 
 def number_keys(a_dictionary):
+    """
+    The function calculates the number of keys in a dictionary.
+
+    :param a_dictionary: The dictionary to calculate the number of keys.
+
+    :return: The number of keys.
+    """
     return len(a_dictionary.keys())
