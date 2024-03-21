21250398cad8a6d21f8176047e082adf69d61efc
Abel Tadesse <138736916+abimtad@users.noreply.github.com>
2024-03-21 12:33:06 +0300

    Update 7-update_dictionary.py

--- 0x04-python-more_data_structures/7-update_dictionary.py
+++ 0x04-python-more_data_structures/7-update_dictionary.py
@@ -1,5 +1,16 @@
 #!/usr/bin/python3
 
 def update_dictionary(a_dictionary, key, value):
-    a_dictionary[key] = value
+    """
+    The function updates a dictionary with the provided key and value pair.
+
+    :param a_dictionary: The dictionary to update.
+    :param key: Dict key.
+    :param value: Dict value.
+
+    :return: Updated dictionary.
+    """
+    if isinstance(a_dictionary, dict):
+        a_dictionary[key] = value
+
     return a_dictionary
