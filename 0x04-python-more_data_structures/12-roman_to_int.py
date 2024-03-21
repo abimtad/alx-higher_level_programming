ae81d5a0944ea50a95043229c77cd9b98ca3356b
Abel Tadesse <138736916+abimtad@users.noreply.github.com>
2024-03-21 23:52:19 +0300

    Update 12-roman_to_int.py

--- 0x04-python-more_data_structures/12-roman_to_int.py
+++ 0x04-python-more_data_structures/12-roman_to_int.py
@@ -1,25 +1,33 @@
 #!/usr/bin/python3
 
 def roman_to_int(roman_string):
-    if not isinstance(roman_string, str) or roman_string is None:
+    """
+    The function coverts a roman numeral into its arabic numeral counterpart.
+
+    :param roman_string: A roman number, represents up to 3999.
+
+    :return: Arabic counterpart of the roman numeral.
+    """
+    if not isinstance(roman_string, str) and roman_string is None:
         return 0
 
-    roman_int = {
-            'I': 1,
-            'V': 5,
-            'X': 10,
-            'L': 50,
-            'C': 100,
-            'D': 500,
-            'M': 1000
-            }
+    roman_numeral = {
+                    'I': 1,
+                    'V': 5,
+                    'X': 10,
+                    'L': 50,
+                    'C': 100,
+                    'D': 500,
+                    'M': 1000
+                    }
+    arabic_numeral = 0
+    length = len(roman_string)
+    for i in range(length):
+        value = roman_numeral.get(roman_string[i])
 
-    convert_int = 0
-    tmp_int = 0
-    for i in reversed(roman_string):
-        if roman_int[i] < tmp_int:
-            convert_int -= roman_int[i]
+        if i < length - 1 and value < roman_numeral.get(roman_string[i + 1]):
+            arabic_numeral -= value
         else:
-            convert_int += roman_int[i]
-        tmp_int = roman_int[i]
-    return convert_int
+            arabic_numeral += value
+
+    return arabic_numeral
