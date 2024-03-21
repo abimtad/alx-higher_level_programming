61d79ffa11f0bb076e720dcddb962035faa956cd
Abel Tadesse <138736916+abimtad@users.noreply.github.com>
2024-03-21 14:12:25 +0300

    Update 9-multiply_by_2.py

--- 0x04-python-more_data_structures/9-multiply_by_2.py
+++ 0x04-python-more_data_structures/9-multiply_by_2.py
@@ -1,6 +1,11 @@
 #!/usr/bin/python3
+
 def multiply_by_2(a_dictionary):
-    anew_dictionary = {}
-    for a, b in a_dictionary.items():
-        anew_dictionary[a] = b * 2
-    return anew_dictionary
+    """
+    The function multiplies all the values of the dictionary by two.
+
+    :param a_dictionary: A dictionary whose values are all integers.
+
+    :return: A dictionary whose values all are multiplied by two.
+    """
+    return {key: value * 2 for key, value in a_dictionary.items()}
