c1331f23647d7983ddff7b23a06659962e4bdf2a
Abel Tadesse <138736916+abimtad@users.noreply.github.com>
2024-03-21 13:57:45 +0300

    Update 8-simple_delete.py

--- 0x04-python-more_data_structures/8-simple_delete.py
+++ 0x04-python-more_data_structures/8-simple_delete.py
@@ -1,7 +1,15 @@
 #!/usr/bin/python3
+
 def simple_delete(a_dictionary, key=""):
-    if key not in a_dictionary.keys():
-        return a_dictionary
-    else:
+    """
+    The function deletes an item from a dictionary with a provided key.
+
+    :param a_dictionary: A dictionary.
+    :param key: A key.
+
+    :return: A dictionary whose item is deleted or intact dictionary.
+    """
+    if key in a_dictionary:
         del a_dictionary[key]
-        return a_dictionary
+
+    return a_dictionary
