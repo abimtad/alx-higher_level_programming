608d6fd462ba66106811ed4631b5b0432f2596be
Abel Tadesse <138736916+abimtad@users.noreply.github.com>
2024-03-21 12:17:08 +0300

    Update 6-print_sorted_dictionary.py

--- 0x04-python-more_data_structures/6-print_sorted_dictionary.py
+++ 0x04-python-more_data_structures/6-print_sorted_dictionary.py
@@ -1,5 +1,19 @@
 #!/usr/bin/python3
 
 def print_sorted_dictionary(a_dictionary):
-    for j in sorted(a_dictionary.keys()):
-        print('{}: {}'.format(j, a_dictionary[j]))
+    """
+    The function prints the key-value paris of a dictionary each on a new line,
+    with alphabetic order of keys.
+
+    :param a_dictionary: A dictionary.
+
+    :return: None.
+    
+    :description: The function avoids printing a newline at the end -
+    of the last output.
+    """
+    sorted_keys = sorted(a_dictionary.keys())
+    for idx, key in enumerate(sorted_keys):
+        if idx:
+            print()
+        print("{0:s}: {1:}".format(key, a_dictionary[key]), end='')
