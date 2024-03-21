ed862ffa9546578cfd0ff0f34cc66458024e9079
Abel Tadesse <138736916+abimtad@users.noreply.github.com>
2024-03-21 11:38:07 +0300

    Update 4-only_diff_elements.py

--- 0x04-python-more_data_structures/4-only_diff_elements.py
+++ 0x04-python-more_data_structures/4-only_diff_elements.py
@@ -1,4 +1,12 @@
 #!/usr/bin/python3
 
 def only_diff_elements(set_1, set_2):
-    return ((set_1 - set_2) ^ (set_2 - set_1))
+    """
+    The function creates a set of the symmetric difference of two sets.
+
+    :param set_1: The first set.
+    :param set_2: The second set.
+
+    :return: a new set of the symmetric difference of two sets.
+    """
+    return set_1 ^ set_2
