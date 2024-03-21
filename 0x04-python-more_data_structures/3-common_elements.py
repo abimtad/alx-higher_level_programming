38472ec6c828381cbb17d9b6c1d2221dd6224ade
Abel Tadesse <138736916+abimtad@users.noreply.github.com>
2024-03-21 11:29:55 +0300

    Update 3-common_elements.py

--- 0x04-python-more_data_structures/3-common_elements.py
+++ 0x04-python-more_data_structures/3-common_elements.py
@@ -1,4 +1,12 @@
 #!/usr/bin/python3
 
 def common_elements(set_1, set_2):
-    return (set_1 & set_2)
+    """
+    The function computes the set of common elements of two sets.
+
+    :param set_1: The first set.
+    :param set_2: The second set.
+
+    :return: A new set of common elements of both sets.
+    """
+    return set_1 & set_2
