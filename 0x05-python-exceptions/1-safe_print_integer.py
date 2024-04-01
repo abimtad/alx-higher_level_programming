7997ee1fc6ac86e2f5b39db9f9f67adcb3825928
Abel Tadesse <138736916+abimtad@users.noreply.github.com>
2024-04-02 00:44:38 +0300

    Update 1-safe_print_integer.py

--- 0x05-python-exceptions/1-safe_print_integer.py
+++ 0x05-python-exceptions/1-safe_print_integer.py
@@ -11,9 +11,8 @@ def safe_print_integer(value):
     :return: True if integer, False otherwise.
     """
     try:
-        integer = int(value)
         print("{:d}".format(integer))
         return True
-    except ValueError:
+    except TypeError:
         return False
     
