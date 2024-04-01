3bed66bfc469ba12d6dcdaaf7ae5d42862819687
Abel Tadesse <tadesseabel26@gmail.com>
2024-04-02 00:28:57 +0300

    task-1

---
+++ 0x05-python-exceptions/1-safe_print_integer.py
@@ -0,0 +1,19 @@
+#!/usr/bin/python3
+
+def safe_print_integer(value):
+    """
+    The function prints only an integer value if sth else is provided -
+    it won't print instead it returns False, True and prints the value -
+    if integer is provided.
+
+    :param value: The value to print.
+
+    :return: True if integer, False otherwise.
+    """
+    try:
+        integer = int(value)
+        print("{:d}".format(integer))
+        return True
+    except ValueError:
+        return False
+    
