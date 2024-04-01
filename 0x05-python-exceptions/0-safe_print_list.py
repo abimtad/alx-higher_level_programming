aeca38860d17c47ed9639a2f87cc93c7e01f36ce
Abel Tadesse <tadesseabel26@gmail.com>
2024-04-01 23:48:24 +0300

    create: 0-safe_print_list.py

---
+++ 0x05-python-exceptions/0-safe_print_list.py
@@ -0,0 +1,22 @@
+#!/usr/bin/pyhton3
+
+def safe_print_list(my_list=[], x=0):
+    """
+    The function print a specific number of element form the -
+    list without space followed a newline.
+
+    :param my_list: The list to print the items from.
+    :param x: The number of items to print to the stdout.
+
+    :return: The actual number of items printed to the stdout.
+    """
+    try:
+        printed_elements = 0
+        for i in range(x):
+            print(my_list[i], end="")
+            printed_elements += 1
+        print()
+    except IndexError:
+        pass
+    finally:
+        return printed_elements
