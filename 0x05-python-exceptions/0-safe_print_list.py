28b013022479160fa487abd0008bd3a1bd101826
Abel Tadesse <138736916+abimtad@users.noreply.github.com>
2024-04-01 23:49:56 +0300

    Update 0-safe_print_list.py

--- 0x05-python-exceptions/0-safe_print_list.py
+++ 0x05-python-exceptions/0-safe_print_list.py
@@ -15,8 +15,8 @@ def safe_print_list(my_list=[], x=0):
         for i in range(x):
             print(my_list[i], end="")
             printed_elements += 1
-        print()
     except IndexError:
         pass
     finally:
+        print()
         return printed_elements
