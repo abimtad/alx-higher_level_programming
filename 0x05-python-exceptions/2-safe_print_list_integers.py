1d698005ff7031e60ec668501313f0cdf6884f35
Abel Tadesse <tadesseabel26@gmail.com>
2024-04-02 11:48:11 +0300

    change the whole logic and add docstring.

--- 0x05-python-exceptions/2-safe_print_list_integers.py
+++ 0x05-python-exceptions/2-safe_print_list_integers.py
@@ -2,21 +2,22 @@
 
 def safe_print_list_integers(my_list=[], x=0):
     """
-    print x int in a list.
+    The function prints 'x' number of integer elements from a list.
 
-    Args:
-        my_list (list): The list to be iterated
-        x (int): The number of elements
+    :param my_list: The list to print the integers form.
+    :param x: The number of integers to print from the list.
 
-    Returns:
-        elements printed
+    :return: The actual number of integers printed to the stdout.
     """
-    k = 0
-    for i in range(x):
-        try:
-            print("{:d}".format(my_list[i]), end="")
-            k += 1
-        except (ValueError, TypeError):
-            continue
-    print("")
-    return k
+    count = 0  # Counter for the number of integers printed
+
+    try:
+        for i in range(x):
+            if isinstance(my_list[i], int):
+                print("{:d}".format(my_list[i]), end="")
+                count += 1
+        print()
+    except IndexError:
+        raise
+
+    return count
