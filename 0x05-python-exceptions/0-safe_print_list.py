268386d0e33d235385a97ef238e2c42e75473cb3
Abel Tadesse <tadesseabel26@gmail.com>
2024-04-02 11:46:04 +0300

    refactore the code.

--- 0x05-python-exceptions/0-safe_print_list.py
+++ 0x05-python-exceptions/0-safe_print_list.py
@@ -15,7 +15,7 @@ def safe_print_list(my_list=[], x=0):
         for i in range(x):
             print("{0:}".format(my_list[i]), end="")
             printed_elements += 1
-    except Exception:
+    except IndexError:
         pass
     finally:
         print()
