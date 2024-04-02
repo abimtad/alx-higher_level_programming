0ac2f18231f1d0e2ae36e7390cee5b1250c81801
Abel Tadesse <tadesseabel26@gmail.com>
2024-04-02 16:50:17 +0300

    change a logic

--- 0x05-python-exceptions/6-raise_exception_msg.py
+++ 0x05-python-exceptions/6-raise_exception_msg.py
@@ -3,7 +3,4 @@
 
 def raise_exception_msg(message=""):
     """raises a name exception with a message."""
-    try:
-        raise NameError
-    except NameError:
-        print(message)
+    raise NameError(message)
