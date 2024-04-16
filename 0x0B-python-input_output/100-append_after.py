6047eb4a67c36fdc24d654c2959c7f469003efc1
Abel Tadesse <tadesseabel26@gmail.com>
2024-04-17 01:25:00 +0300

    update files

--- 0x0B-python-input_output/100-append_after.py
+++ 0x0B-python-input_output/100-append_after.py
@@ -1,10 +1,10 @@
 #!/usr/bin/python3
-"""Defines text appender function."""
+"""Defines text appending function."""
 
 
 def append_after(filename="", search_string="", new_string=""):
-    """inserts a line to a file, after each line containing a -
-    specific string.
+    """inserts a line to a file, after each line containing -
+    a specific string.
     """
     with open(filename, 'r+', encode='utf-8') as f:
         for line in f.readlines():
