19a21c0d251e89593a43b85b28d4d125a14c1ded
Abel Tadesse <tadesseabel26@gmail.com>
2024-07-05 00:41:01 +0300

    task_5

--- 0x10-python-network_0/5-post_params.sh
+++ 0x10-python-network_0/5-post_params.sh
@@ -1,3 +1,6 @@
 #!/bin/bash
 # sends POST request with data
-curl -sL "$1" -X POST -d "email=test@gmail.com&subject=I will always be here for PLD"
+# curl -sL "$1" -X POST -d "email=test@gmail.com&subject=I will always be here for PLD"
+#!/bin/bash
+# Bash scripts that sends a POST request to a given URL.
+curl -s -X POST -d "email=test@gmail.com&subject=I will always be here for PLD" "$1"
