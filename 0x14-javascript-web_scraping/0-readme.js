d2b3b3324c379cded7a5cce5a57878204319ad35
Abel Tadesse <tadesseabel26@gmail.com>
2024-08-13 02:30:09 +0300

    fix

--- 0x14-javascript-web_scraping/0-readme.js
+++ 0x14-javascript-web_scraping/0-readme.js
@@ -1,13 +1,12 @@
 #!/usr/bin/node
-// This script accepts a file path as an argument and reads
-// the contents of the file and prints it to the console.
-const fs = require('fs');
-const file = process.argv[2];
+// Read from file
 
-fs.readFile(file, 'utf8', (err, data) => {
-  if (err) {
-    console.log(err);
-  } else {
+const filesys = require('fs');
+filesys.readFile(process.argv[2], 'utf-8',
+  function (err, data) {
+    if (err) {
+      console.log(err);
+      return;
+    }
     console.log(data);
-  }
-});
+  });
