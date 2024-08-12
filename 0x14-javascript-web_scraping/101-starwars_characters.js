483214c85d8216c37dfbaf428a44b413e175741d
Abel Tadesse <138736916+abimtad@users.noreply.github.com>
2024-08-13 02:41:30 +0300

    Update 101-starwars_characters.js

--- 0x14-javascript-web_scraping/101-starwars_characters.js
+++ 0x14-javascript-web_scraping/101-starwars_characters.js
@@ -1,19 +1,25 @@
 #!/usr/bin/node
-const req = require('request');
-req('https://swapi-api.hbtn.io/api/films/' + process.argv[2], function (error, response, body) {
-  if (!error) {
-    const characters = JSON.parse(body).characters;
-    orderprint(characters, 0);
+
+const request = require('request');
+const id = process.argv[2];
+const url = `https://swapi-api.alx-tools.com/api/films/${id}`;
+
+request.get(url, (error, response, body) => {
+  if (error) {
+    console.log(error);
+  } else {
+    const content = JSON.parse(body);
+    const characters = content.characters;
+    // console.log(characters);
+    for (const character of characters) {
+      request.get(character, (error, response, body) => {
+        if (error) {
+          console.log(error);
+        } else {
+          const names = JSON.parse(body);
+          console.log(names.name);
+        }
+      });
+    }
   }
 });
-
-function orderprint (characters, index) {
-  req(characters[index], function (error, response, body) {
-    if (!error) {
-      console.log(JSON.parse(body).name);
-      if (index + 1 < characters.length) {
-        orderprint(characters, index + 1);
-      }
-    }
-  });
-}
