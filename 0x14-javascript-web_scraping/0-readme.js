7bfc99c7b1c707dc0478a542938f87ab98587f0f
Abel Tadesse <138736916+abimtad@users.noreply.github.com>
2024-08-13 02:37:19 +0300

    Update 0-readme.js

--- 0x14-javascript-web_scraping/0-readme.js
+++ 0x14-javascript-web_scraping/0-readme.js
@@ -1,19 +1,12 @@
 #!/usr/bin/node
 // Read from file
 
-const fs = require('fs');
-
-const filePath = process.argv[2];
-
-if (!filePath) {
-  console.log('Usage: node script.js <file_path>');
-  process.exit(1);
-}
-
-fs.readFile(filePath, 'utf-8', (err, data) => {
-  if (err) {
-    console.error('Error:', err);
-    return;
-  }
-  console.log(data);
-});
+const filesys = require('fs');
+filesys.readFile(process.argv[2], 'utf-8',
+  function (err, data) {
+    if (err) {
+      console.log(err);
+      return;
+    }
+    console.log(data);
+  });
