caa5d85ca852d0b266ebf742174d04d9d82b79f8
Abel Tadesse <138736916+abimtad@users.noreply.github.com>
2024-08-13 02:39:21 +0300

    Update 1-writeme.js

--- 0x14-javascript-web_scraping/1-writeme.js
+++ 0x14-javascript-web_scraping/1-writeme.js
@@ -1,5 +1,10 @@
 #!/usr/bin/node
-const writeme = require('fs');
-writeme.writeFile(process.argv[2], process.argv[3], function (error) {
-  if (error) console.log(error);
-});
+
+const fs = require('fs');
+
+fs.writeFile(process.argv[2], process.argv[3], 'utf-8',
+  function (err) {
+    if (err) {
+      console.log(err);
+    }
+  });
